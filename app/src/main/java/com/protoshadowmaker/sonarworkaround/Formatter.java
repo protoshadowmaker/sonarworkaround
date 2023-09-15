0f07ebafdef9681513bc426e074550eb054e475f
protoshadowmaker <protoshadowmaker@proton.me>
2023-09-15 12:41:20 +0200

    Reduce dependencies

--- app/src/main/java/com/protoshadowmaker/sonarworkaround/Formatter.java
+++ app/src/main/java/com/protoshadowmaker/sonarworkaround/Formatter.java
@@ -1,10 +1,8 @@
 package com.protoshadowmaker.sonarworkaround;
 
-import androidx.annotation.NonNull;
-
 public class Formatter {
-    @NonNull
-    public String format(@NonNull String src) {
+
+    public String format(String src) {
         return "Formatted: " + src;
     }
 }
